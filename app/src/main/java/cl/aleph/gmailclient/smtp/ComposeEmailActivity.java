2368e6dd05df89f427b7712829fcdf6946fbc21b
César Parra <cesar.parramoreno@gmail.com>
2016-07-04 15:31:11 -0400

    Form's validation added. [#121758989]

--- app/src/main/java/cl/aleph/gmailclient/smtp/ComposeEmailActivity.java
+++ app/src/main/java/cl/aleph/gmailclient/smtp/ComposeEmailActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
@@ -17,33 +18,51 @@ public class ComposeEmailActivity extends AppCompatActivity {
     private SendEmailTask sendEmailTask;
     private SharedPreferences userPreferences;
     private EditText emailTo;
+    private EditText subject;
+    private EditText data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose_email);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.compose_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.send_email);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendEmail();
+                String from = userPreferences.getString(LoginActivity.USER_EMAIL, null);
+                String password = userPreferences.getString(LoginActivity.USER_PASSWORD, null);
+                String to = emailTo.getText().toString();
+                String _subject = subject.getText().toString();
+                String _data = data.getText().toString();
+                if (from == null || password == null) {
+                    //ToDO: send to login activity
+                }
+                if (TextUtils.isEmpty(to)) {
+                    emailTo.setError(getString(R.string.error_field_required));
+                    emailTo.requestFocus();
+                }
+                if (TextUtils.isEmpty(_subject)) {
+                    subject.setError(getString(R.string.error_field_required));
+                    subject.requestFocus();
+                }
+                if (TextUtils.isEmpty(_data)) {
+                    data.setError(getString(R.string.error_field_required));
+                    data.requestFocus();
+                }
+                sendEmail(from, to, password, _data, _subject);
 
             }
         });
         emailTo = (EditText) findViewById(R.id.compose_email_to);
+        subject = (EditText) findViewById(R.id.compose_email_subject);
+        data = (EditText) findViewById(R.id.compose_email_data);
         userPreferences = getSharedPreferences(LoginActivity.USER_PREFERENCES, Context.MODE_PRIVATE);
     }
 
-    private void sendEmail() {
-        String from = userPreferences.getString(LoginActivity.USER_EMAIL, null);
-        String password = userPreferences.getString(LoginActivity.USER_PASSWORD, null);
-        String to = emailTo.getText().toString();
-        if (from == null || password == null) {
-            //ToDO: display some errors
-        }
-        sendEmailTask = new SendEmailTask(from, to, password, this);
+    private void sendEmail(String from, String to, String password, String _data, String _subject) {
+        sendEmailTask = new SendEmailTask(from, to, password, _data, _subject, this);
         sendEmailTask.execute((Void) null);
     }
 
